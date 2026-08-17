2824e28299e7d6910714077f302af1e07c17191f
owilliamdepaula <williamfcodepaula@hotmail.com>
2026-08-17 20:50:15 -0300

    feat: Adição data de nascimento e idade

--- Domínio/Dvd.cs
+++ Domínio/Dvd.cs
@@ -1,16 +1,5 @@
 namespace Biblioteca.Dominio;
 
-public class Dvd(string titulo, string autor, ClassificacaoEtaria classificacao) : ItemAcervo(titulo, autor)
-{
-    public ClassificacaoEtaria Classificacao { get; } = classificacao;
-
-    public bool PodeSerEmprestadoPara(Locador locador);
-
-    public override int PrazoDevolucao => 3;
-
-    public override decimal MultaDiaAtrasado => 3m;
-}
-
 public enum ClassificacaoEtaria
 {
     Livre = 0,
@@ -18,4 +7,37 @@ public enum ClassificacaoEtaria
     CatorzeAnos = 14,
     DezesseisAnos = 16,
     DezoitoAnos = 18
+}
+
+public class Dvd(string titulo, string autor, ClassificacaoEtaria classificacao) : ItemAcervo(titulo, autor)
+
+{
+    public ClassificacaoEtaria Classificacao { get; } = classificacao;
+
+    public override int PrazoDevolucao => 3;
+
+    public override decimal MultaDiaAtrasado => 3m;
+
+    public bool PodeSerEmprestadoPara(Locador locador)
+    {
+        if (Classificacao == ClassificacaoEtaria.Livre)
+        {
+            return true;
+        }
+        int idade = CalcularIdade(locador.DataNascimento);
+        return idade >= (int)Classificacao;
+    }
+
+    public int CalcularIdade(DateTime dataNascimento)
+    {
+        DateTime hoje = DateTime.Today;
+        int idade = hoje.Year - dataNascimento.Year;
+
+        //se a pessoa ainda não fez aniversário nesse ano, retirar um ano da idade calculada.
+        if (dataNascimento.Date > hoje.AddYears(-idade))
+        {
+            idade--;
+        }
+        return idade;
+    }
 }
--- Domínio/Locador.cs
+++ Domínio/Locador.cs
@@ -4,5 +4,10 @@ public class Locador
 {
     public string Nome { get; set; }
     public DateTime DataNascimento { get; set; }
+
     public Locador(string nome, DateTime dataNascimento)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+    }
 }
