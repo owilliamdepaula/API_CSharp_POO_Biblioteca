a06715bd8f463d79abbe4f1bbd886b31b37de25d
owilliamdepaula <williamfcodepaula@hotmail.com>
2026-08-20 18:13:46 -0300

    feat: correções

--- Biblioteca.Dominio/Emprestimo.cs
+++ Biblioteca.Dominio/Emprestimo.cs
@@ -2,30 +2,56 @@ namespace Biblioteca.Dominio;
 
 public class Emprestimo
 {
-    public ItemAcervo Item { get; }
+    public ItemAcervo Item { get; private set; }
+
+    public Locador locador { get; private set; }
+
     public DateTime DataEmprestimo { get; private set; } = DateTime.Today;
+
     public DateTime PrazoLimite { get; }
+
+    public DateTime? DataDevolucao { get; private set; }
+
+    public bool Devolvido => DataDevolucao.HasValue;
+
+    private decimal? _multaCongelada;
+
     
-    public Emprestimo(ItemAcervo item)
+
+    public Emprestimo(ItemAcervo item, Cliente cliente)
     {
+          if (!item.PodeSerEmprestadoPara(cliente))
+        {
+            throw new ExcecaoDominio("Cliente não pode pq é menor...");
+        }
+
+        if (cliente.EmprestimosAtivos.Count() >= 3)
+        {
+            throw new ExcecaoDominio("Explodiu o limite 3 itens emprestados.");
+        }
         item.MarcarComoEmprestado();
         Item = item;
+        Cliente = cliente;
         PrazoLimite = DataEmprestimo.AddDays(item.PrazoDevolucao);
+        cliente.Emprestimos.Add(this);
+
     }
 
-    public decimal MultaAtual => Item.CalcularMulta(QtdDiasAtrasados);
-    public int QtdDiasAtrasados
+    public decimal MultaAtual => Item.CalcularMulta(QtDiasAtrasados);
+    public int QtDiasAtrasados
     {
         get
         {
-            TimeSpan diasAtrasados = DateTime.Today - PrazoLimite;
+            DateTime referencia = DataDevolucao ?? DateTime.Today;
+            TimeSpan diasAtrasados = referencia - PrazoLimite;
             return diasAtrasados.Days;
+
         }
     }
-
     public void RegistrarDevolucao()
     {
         Item.MarcarComoDevolvido();
+         DataDevolucao = DateTime.Today;
+        _multaCongelada = Item.CalcularMulta(QtDiasAtrasados);
     }
-
 }
--- Biblioteca.Dominio/Locador.cs
+++ Biblioteca.Dominio/Locador.cs
@@ -10,4 +10,24 @@ public class Locador
         Nome = nome;
         DataNascimento = dataNascimento;
     }
+    public string Nome { get; }
+
+
+    public DateTime DataNascimento { get; }
+    public int Idade
+    {
+        get
+        {
+            var hoje = DateTime.Today;
+            var idade = hoje.Year - DataNascimento.Year;
+
+
+            if (DataNascimento.Date > hoje.AddYears(-idade))
+                idade--;
+
+            return idade;
+        }
+    }
+    public List<Emprestimo> Emprestimos { get; } = new();
+    public IEnumerable<Emprestimo> EmprestimosAtivos => Emprestimos.Where(e => !e.Devolvido);
 }
