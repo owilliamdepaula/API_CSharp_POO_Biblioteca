2219d25a8ca6eb0c519dff8d469bbda3a82170e2
owilliamdepaula <williamfcodepaula@hotmail.com>
2026-08-20 18:39:42 -0300

    feat: correções2

--- Biblioteca.Laboratorio/Program.cs
+++ Biblioteca.Laboratorio/Program.cs
@@ -1,11 +1,32 @@
 ﻿using Biblioteca.Dominio;
 
-Console.WriteLine("Hello, World!");
-
-ItemAcervo LivrodeEli = new Dvd("Livro de Eli", "Allen Hughes", default(ClassificacaoEtaria));
-
+ItemAcervo LivrodeEli = new Dvd("Livro de Eli", "Allen Hughes");
 ItemAcervo VocêéInsubstituível = new Livro("Você é Insubstituível", "Augusto Cury");
-
-Emprestimo Divida = new Emprestimo(LivrodeEli);
+ItemAcervo TriploX = new Dvd("TriploX", "Vin Diesel", 18);
+ItemAcervo Fluir = new Revista("Fluir", "Editora Terra, Mar e Ar");
+ItemAcervo VelozesEFuriosos = new Dvd("Sharknado", "sharkão", 16);
+ItemAcervo AArteDaGuerra = new Livro("A arte da Guerra", "Sun Tzu");
+ItemAcervo AVoltaDosQueNãoForam = new Dvd ("A volta dos que não foram", "Autor Foi", 0);
 
 Divida.RegistrarDevolucao();
+
+
+Locador locador1 = new Cliente("João Silva", new DateTime(2005, 3, 15));
+Locador locador2 = new Cliente("Maria Santos", new DateTime(2000, 7, 22));
+Locador locador3 = new Cliente("Pedro Oliveira", new DateTime(2008, 11, 8));
+
+Emprestimo emprestimo = new Emprestimo(AArteDaGuerra, locador1);
+Emprestimo emprestimo1 = new Emprestimo(TriploX, locador2);
+Emprestimo emprestimo2 = new Emprestimo(Fluir, locador3);
+
+
+emprestimo.RegistrarDevolucao();
+emprestimo1.RegistrarDevolucao();
+emprestimo2.RegistrarDevolucao();
+
+locador1 locador = new Locador(
+    "William",
+    new DateTime(2000, 5, 10)
+);
+
+Console.WriteLine(cliente.Idade);
